eefb3a6ea9f8c877d739ec88639f505288fda222
rondo-v <rwondov@gmail.com>
2025-10-26 21:57:34 +0100

    modified:   manifest.jps modified:   scripts/memoryConfig.sh modified:   scripts/start-server.sh

--- scripts/memoryConfig.sh
+++ scripts/memoryConfig.sh
@@ -1,18 +1,18 @@
 #!/bin/bash        
 
-if ! `echo $JVM_OPTS | grep -q "\-Xms[[:digit:]\.]"`
+if ! `echo $JAVA_OPTS | grep -q "\-Xms[[:digit:]\.]"`
 then
         [ -z "$XMS" ] && { XMS="-Xms32M"; }
-        JVM_OPTS=$JVM_OPTS" $XMS"; 
+        JAVA_OPTS=$JAVA_OPTS" $XMS"; 
 fi
 
-if ! `echo $JVM_OPTS | grep -q "\-Xmn[[:digit:]\.]"`
+if ! `echo $JAVA_OPTS | grep -q "\-Xmn[[:digit:]\.]"`
 then
         [ -z "$XMN" ] && { XMN="-Xmn30M"; }
-        JVM_OPTS=$JVM_OPTS" $XMN"; 
+        JAVA_OPTS=$JAVA_OPTS" $XMN"; 
 fi
 
-if ! `echo $JVM_OPTS | grep -q "\-Xmx[[:digit:]\.]"`
+if ! `echo $JAVA_OPTS | grep -q "\-Xmx[[:digit:]\.]"`
 then
         [ -z "$XMX" ] && {
         	#optimal XMX = 80% * total available RAM
@@ -21,19 +21,19 @@ then
         	let XMX=memory_total*8/10;
         	XMX="-Xmx${XMX}M";
         }
-        JVM_OPTS=$JVM_OPTS" $XMX";
+        JAVA_OPTS=$JAVA_OPTS" $XMX";
 fi
 
-if ! `echo $JVM_OPTS | grep -q "\-Xminf[[:digit:]\.]"`
+if ! `echo $JAVA_OPTS | grep -q "\-Xminf[[:digit:]\.]"`
 then
         [ -z "$XMINF" ] && { XMINF="-Xminf0.1"; }
-        JVM_OPTS=$JVM_OPTS" $XMINF"; 
+        JAVA_OPTS=$JAVA_OPTS" $XMINF"; 
 fi
 
-if ! `echo $JVM_OPTS | grep -q "\-Xmaxf[[:digit:]\.]"`
+if ! `echo $JAVA_OPTS | grep -q "\-Xmaxf[[:digit:]\.]"`
 then
         [ -z "$XMAXF" ] && { XMAXF="-Xmaxf0.3"; }
-        JVM_OPTS=$JVM_OPTS" $XMAXF"; 
+        JAVA_OPTS=$JAVA_OPTS" $XMAXF"; 
 fi
 
 XMX_VALUE=`echo $XMX | grep -o "[0-9]*"`;
@@ -42,15 +42,11 @@ if [[ $XMX_UNIT == "g" ]] || [[ $XMX_UNIT == "G" ]] ; then
 	let XMX_VALUE=$XMX_VALUE*1024; 
 fi
 
-JAVA_AGENT="-javaagent:/data/jelastic-gc-agent.jar=period=300"
-JVM_OPTS=$JVM_OPTS" $JAVA_AGENT";
-
 JAVA_VERSION=$(java -version 2>&1 | grep version |  awk -F '.' '{print $2}')
    
-if ! `echo $JVM_XX_OPTS | grep -q "UseCompressedOops"`
+if ! `echo $JAVA_OPTS | grep -q "UseCompressedOops"`
 then
-    	JVM_XX_OPTS=$JVM_XX_OPTS" -XX:+UseCompressedOops"
+    	JAVA_OPTS=$JAVA_OPTS" -XX:+UseCompressedOops"
 fi
 
-export JVM_OPTS
-export JVM_XX_OPTS
+export JAVA_OPTS
