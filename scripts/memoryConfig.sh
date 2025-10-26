a088749632bc5c41b375f8043d3490444befb894
rondo-v <rwondov@gmail.com>
2025-10-26 18:39:09 +0100

    modified: manifest.jps scripts/memoryConfig.sh

--- scripts/memoryConfig.sh
+++ scripts/memoryConfig.sh
@@ -1,18 +1,18 @@
 #!/bin/bash        
 
-if ! `echo $JAVA_OPTS | grep -q "\-Xms[[:digit:]\.]"`
+if ! `echo $JVM_OPTS | grep -q "\-Xms[[:digit:]\.]"`
 then
         [ -z "$XMS" ] && { XMS="-Xms32M"; }
-        JAVA_OPTS=$JAVA_OPTS" $XMS"; 
+        JVM_OPTS=$JVM_OPTS" $XMS"; 
 fi
 
-if ! `echo $JAVA_OPTS | grep -q "\-Xmn[[:digit:]\.]"`
+if ! `echo $JVM_OPTS | grep -q "\-Xmn[[:digit:]\.]"`
 then
         [ -z "$XMN" ] && { XMN="-Xmn30M"; }
-        JAVA_OPTS=$JAVA_OPTS" $XMN"; 
+        JVM_OPTS=$JVM_OPTS" $XMN"; 
 fi
 
-if ! `echo $JAVA_OPTS | grep -q "\-Xmx[[:digit:]\.]"`
+if ! `echo $JVM_OPTS | grep -q "\-Xmx[[:digit:]\.]"`
 then
         [ -z "$XMX" ] && {
         	#optimal XMX = 80% * total available RAM
@@ -21,19 +21,19 @@ then
         	let XMX=memory_total*8/10;
         	XMX="-Xmx${XMX}M";
         }
-        JAVA_OPTS=$JAVA_OPTS" $XMX";
+        JVM_OPTS=$JVM_OPTS" $XMX";
 fi
 
-if ! `echo $JAVA_OPTS | grep -q "\-Xminf[[:digit:]\.]"`
+if ! `echo $JVM_OPTS | grep -q "\-Xminf[[:digit:]\.]"`
 then
         [ -z "$XMINF" ] && { XMINF="-Xminf0.1"; }
-        JAVA_OPTS=$JAVA_OPTS" $XMINF"; 
+        JVM_OPTS=$JVM_OPTS" $XMINF"; 
 fi
 
-if ! `echo $JAVA_OPTS | grep -q "\-Xmaxf[[:digit:]\.]"`
+if ! `echo $JVM_OPTS | grep -q "\-Xmaxf[[:digit:]\.]"`
 then
         [ -z "$XMAXF" ] && { XMAXF="-Xmaxf0.3"; }
-        JAVA_OPTS=$JAVA_OPTS" $XMAXF"; 
+        JVM_OPTS=$JVM_OPTS" $XMAXF"; 
 fi
 
 XMX_VALUE=`echo $XMX | grep -o "[0-9]*"`;
@@ -42,9 +42,12 @@ if [[ $XMX_UNIT == "g" ]] || [[ $XMX_UNIT == "G" ]] ; then
 	let XMX_VALUE=$XMX_VALUE*1024; 
 fi
 
+JAVA_AGENT = "-javaagent:/data/jelastic-gc-agent.jar=period=300"
+JVM_OPTS=$JVM_OPTS" $JAVA_AGENT";
+
 JAVA_VERSION=$(java -version 2>&1 | grep version |  awk -F '.' '{print $2}')
 
-if ! `echo $JAVA_OPTS | grep -q "\-XX:MaxPermSize"`
+if ! `echo $JVM_XX_OPTS | grep -q "\-XX:MaxPermSize"`
 then
         [ -z "$MAXPERMSIZE" ] && { 
         	# if java version <= 7 then configure MaxPermSize otherwise ignore 
@@ -56,10 +59,10 @@ then
                 	}
 		}
   	}
-        JAVA_OPTS=$JAVA_OPTS" $MAXPERMSIZE";
+        JVM_XX_OPTS=$JVM_XX_OPTS" $MAXPERMSIZE";
 fi
  
-if ! `echo $JAVA_OPTS | grep -q "\-XX:+Use.*GC"`
+if ! `echo $JVM_XX_OPTS | grep -q "\-XX:+Use.*GC"`
 then	
 	[ -z "$GC" ] && {  
         	[ $JAVA_VERSION -le 7 ] && {
@@ -69,12 +72,13 @@ then
 	    		GC="-XX:+UseG1GC";
 	    	}
      	}
-        JAVA_OPTS=$JAVA_OPTS" $GC"; 
+        JVM_XX_OPTS=$JVM_XX_OPTS" $GC"; 
 fi 
    
-if ! `echo $JAVA_OPTS | grep -q "UseCompressedOops"`
+if ! `echo $JVM_XX_OPTS | grep -q "UseCompressedOops"`
 then
-    	JAVA_OPTS=$JAVA_OPTS" -XX:+UseCompressedOops"
+    	JVM_XX_OPTS=$JVM_XX_OPTS" -XX:+UseCompressedOops"
 fi
 
-export JAVA_OPTS
+export JVM_OPTS
+export JVM_XX_OPTS
