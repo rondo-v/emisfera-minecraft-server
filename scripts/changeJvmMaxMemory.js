340e6262c51595861db366ca731edc67e9d6f28b
rondo-v <rwondov@gmail.com>
2025-10-27 02:00:52 +0100

    modified:   scripts/changeJvmMaxMemory.js

--- scripts/changeJvmMaxMemory.js
+++ scripts/changeJvmMaxMemory.js
@@ -5,7 +5,7 @@ import com.hivext.api.development.Scripting;
 
 var envName = '${env.envName}';
 var count = Math.max(fixedCloudlets, flexibleCloudlets)*128*8/10;
-var resp = jelastic.env.control.AssContainerEnvVars(envName, session, nodeId, '{\"MAX_MEMORY\": \"1024M\"}');
+var resp = jelastic.env.control.AddContainerEnvVars(envName, session, nodeId, '{\"MAX_MEMORY\": \"1024M\"}');
 if (resp.result != 0) return resp;
 
 return {
