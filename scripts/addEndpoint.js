63a7e6cbb0bb7b055c0baefd1b99f780262dd178
rondo-v <rwondov@gmail.com>
2025-10-27 02:17:58 +0100

    modified:   scripts/addEndpoint.js modified:   scripts/changeJvmMaxMemory.js

--- scripts/addEndpoint.js
+++ scripts/addEndpoint.js
@@ -1,18 +1,11 @@
 //@auth
 //@req(nodeId, port)
-
-import com.hivext.api.development.Scripting;
-
 var envName = '${env.envName}';
 
 var resp = jelastic.env.control.AddEndpoint(envName, session, nodeId, port, "TCP", "Minecraft Server");
 if (resp.result != 0) return resp;
 
 var url = "${env.domain}:" + resp.object.publicPort;
-
-var scripting =  hivext.local.exp.wrapRequest(new Scripting({
-    serverUrl : "http://" + window.location.host.replace("app", "appstore") + "/"
-}));
 
 return {
     result: 0,
--- scripts/changeJvmMaxMemory.js
+++ scripts/changeJvmMaxMemory.js
@@ -1,11 +1,9 @@
 //@auth
 //@req(nodeId, port)
-
-import com.hivext.api.development.Scripting;
-
 var envName = '${env.envName}';
 var count = Math.max(fixedCloudlets, flexibleCloudlets)*128*8/10;
-var resp = jelastic.env.control.AddContainerEnvVars(envName, session, nodeId, '{\"MAX_MEMORY\": \"1024M\"}');
+var envs = {"MAX_MEMORY":count};
+var resp = jelastic.env.control.AddContainerEnvVars(envName, session, nodeId, envs);
 if (resp.result != 0) return resp;
 
 return {
