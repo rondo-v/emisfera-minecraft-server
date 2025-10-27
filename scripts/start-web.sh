f7ff9e3a751de57eb159bcfdaeebfe611235d214
rondo-v <rwondov@gmail.com>
2025-10-27 03:20:03 +0100

    deleted:    lib/jelastic-gc-agent.jar modified:   manifest.jps deleted:    properties/server.properties deleted:    scripts/changeJvmMaxMemory.js deleted:    scripts/memoryConfig.sh deleted:    scripts/start-server.sh deleted:    scripts/start-web.sh deleted:    web/css/.gitignore deleted:    web/css/bootstrap.min.css deleted:    web/css/form-style.css deleted:    web/img/.gitignore deleted:    web/img/header.png deleted:    web/img/image00.png deleted:    web/img/image06.png deleted:    web/img/image13.png deleted:    web/index.html

--- scripts/start-web.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i '/cd \/data/a [ -f /data/web/index.html ] && { sed -i "s/VANILLA_VERSION/$VANILLA_VERSION/g" /data/web/index.html; pushd /data/web; screen -md python -m SimpleHTTPServer 8080 ; popd; }' /start-minecraftFinalSetup
-
-
